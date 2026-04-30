973d24a5a082b754cbe498bc5112a36d9cd7d2fb
feweks <gdfeweks@gmail.com>
2026-04-30 21:18:52 +0200

    expanded resource loading

--- src/Client/Resources.cs
+++ src/Client/Resources.cs
@@ -6,7 +6,97 @@ namespace Game.Client;
 
 static class Resources
 {
-    public static void Init() { }
+    private static Dictionary<string, Texture2D> textures = new Dictionary<string, Texture2D>();
+    private static Dictionary<string, Model> models = new Dictionary<string, Model>();
+    private static Dictionary<string, Font> fonts = new Dictionary<string, Font>();
+
+    private static Texture2D errorTex;
+    private static Model errorMdl;
+    private static Font errorFnt;
+
+    public static void Init()
+    {
+        const int ERROR_IMG_SIZE = 100;
+        Image errorImg = Raylib.GenImageChecked(ERROR_IMG_SIZE, ERROR_IMG_SIZE, ERROR_IMG_SIZE / 2, ERROR_IMG_SIZE / 2, Color.Magenta, Color.Black);
+        errorTex = Raylib.LoadTextureFromImage(errorImg);
+        Raylib.UnloadImage(errorImg);
+
+        Mesh errorMesh = Raylib.GenMeshCube(0.5f, 0.5f, 0.5f);
+        errorMdl = Raylib.LoadModelFromMesh(errorMesh);
+        Material errorMat = Raylib.GetMaterial(ref errorMdl, 0);
+        Raylib.SetMaterialTexture(ref errorMat, MaterialMapIndex.Albedo, errorTex);
+
+        errorFnt = Raylib.GetFontDefault();
+    }
+
+    public static Texture2D GetTexture(string path)
+    {
+        if (textures.TryGetValue(path, out Texture2D cacheTex))
+            return cacheTex;
+
+        if (!File.Exists(path))
+        {
+            Raylib.TraceLog(TraceLogLevel.Warning, $"Failed to load texture {path}: file not found");
+            return errorTex;
+        }
+
+        Texture2D tex = Raylib.LoadTexture(path);
+
+        if (!Raylib.IsTextureValid(tex))
+        {
+            Raylib.TraceLog(TraceLogLevel.Warning, $"Failed to load texture {path}: invalid data");
+            return errorTex;
+        }
+
+        textures.Add(path, tex);
+        return tex;
+    }
+
+    public static Model GetModel(string path)
+    {
+        if (models.TryGetValue(path, out Model cacheMdl))
+            return cacheMdl;
+
+        if (!File.Exists(path))
+        {
+            Raylib.TraceLog(TraceLogLevel.Warning, $"Failed to load model {path}: file not found");
+            return errorMdl;
+        }
+
+        Model mdl = Raylib.LoadModel(path);
+
+        if (!Raylib.IsModelValid(mdl))
+        {
+            Raylib.TraceLog(TraceLogLevel.Warning, $"Failed to load model {path}: invalid data");
+            return errorMdl;
+        }
+
+        models.Add(path, mdl);
+        return mdl;
+    }
+
+    public static Font GetFont(string path)
+    {
+        if (fonts.TryGetValue(path, out Font cacheFont))
+            return cacheFont;
+
+        if (!File.Exists(path))
+        {
+            Raylib.TraceLog(TraceLogLevel.Warning, $"Failed to load font {path}: file not found");
+            return errorFnt;
+        }
+
+        Font fnt = Raylib.LoadFontEx(path, 120, null, 400);
+
+        if (!Raylib.IsFontValid(fnt))
+        {
+            Raylib.TraceLog(TraceLogLevel.Warning, $"Failed to load font {path}: invalid data");
+            return errorFnt;
+        }
+
+        fonts.Add(path, fnt);
+        return fnt;
+    }
 
     public static string GetFile(string path)
     {
@@ -38,8 +128,26 @@ static class Resources
         return deserializedData;
     }
 
+    public static void Unload()
+    {
+        foreach (var tex in textures.Values)
+            Raylib.UnloadTexture(tex);
+        textures.Clear();
+
+        foreach (var mdl in models.Values)
+            Raylib.UnloadModel(mdl);
+        models.Clear();
+
+        foreach (var fnt in fonts.Values)
+            Raylib.UnloadFont(fnt);
+        fonts.Clear();
+    }
+
     public static void Shutdown()
     {
+        Unload();
 
+        Raylib.UnloadTexture(errorTex);
+        Raylib.UnloadModel(errorMdl);
     }
 }
