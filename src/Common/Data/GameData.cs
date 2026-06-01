02ca53c3a9c206884f9c37b28298e57be73829e4
feweks <gdfeweks@gmail.com>
2026-06-02 01:18:18 +0200

    version bump

--- src/Common/Data/GameData.cs
+++ src/Common/Data/GameData.cs
@@ -9,5 +9,5 @@ static class GameData
     public static string ServerDataPath => Path.Combine(DataPath, "server_data");
     public static uint LobbyCodeLength => 6;
     public static uint MaxPacketSize => 16384;
-    public static Version Version => new Version(0, 4, 0);
+    public static Version Version => new Version(0, 5, 0);
 }
