e5d5e6c7e6302bf1914c23710a17e0b070e0cbb6
feweks <gdfeweks@gmail.com>
2026-06-28 20:43:41 +0200

    debug container rendering

--- src/Client/Data/UI/Widgets/ContainerUIWidget.cs
+++ src/Client/Data/UI/Widgets/ContainerUIWidget.cs
@@ -8,6 +8,7 @@ class ContainerUIWidget : UIWidget
 {
     public const string NODE_NAME = "container";
     public const string CONTAINER_UI_TYPE_NONE = "none";
+    public const string CONTAINER_UI_TYPE_DEBUG = "debug";
     public const string CONTAINER_UI_TYPE_BOX = "box";
 
     public string Type { get; }
@@ -45,5 +46,8 @@ class ContainerUIWidget : UIWidget
     public override void Draw()
     {
         base.Draw();
+
+        if (Type == CONTAINER_UI_TYPE_DEBUG)
+            Raylib.DrawRectangleLinesEx(GetInteractableArea(), 4f, Color.DarkBlue);
     }
 }
